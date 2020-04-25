90e3d432133078eb69b941ec50b8bd34316ea2e7
Henry Rodman <henry.rodman@gmail.com>
2020-04-25 06:31:36 -0500

    add tests for CSV download functions
    https://app.asana.com/0/1160646149628811/1172186530780729

--- tests/testthat/test-download_FIADB.R
+++ tests/testthat/test-download_FIADB.R
@@ -1,3 +1,32 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("CSV download functions work correctly")
+test_that("download_by_state works properly", {
+  want_tables <- c("PLOT", "TREE", "SUBPLOT", "COND", "SURVEY")
+
+  csv_files <- download_by_state(
+    state = "ND",
+    file_dir = tempdir(),
+    files = want_tables
+  )
+
+  expect_named(csv_files, expected = want_tables)
+  expect_true(
+    all(purrr::map_lgl(csv_files, .f = ~ tools::file_ext(.x) == "csv"))
+  )
+})
+
+test_that("download_and_unzip works properly", {
+  url <- "https://apps.fs.usda.gov/fia/datamart/CSV/VT_PLOT.zip"
+  local_file <- file.path(
+    tempdir(),
+    "VT_PLOT.csv"
+  )
+
+  downloaded <- download_and_unzip(url = url, file_dir = tempdir())
+  expect_length(local_file, n = 1)
+  expect_identical(local_file, downloaded)
+
+  bad_url <- "https://apps.fs.usda.gov/fia/datamart/CSV/ZZ_PLOT.zip"
+  expect_error(
+    download_and_unzip(url = bad_url, file_dir = tempdir())
+  )
 })
